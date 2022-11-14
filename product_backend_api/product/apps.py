fcd9ee0412b90cdcfe874f83ac31c0361273df40
voolyvex <1joeknapp@gmail.com>
2022-11-14 01:44:58 -0600

    include product image field in Product class + create default image url variable in ProductConfig

--- product_backend_api/product/apps.py
+++ product_backend_api/product/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
+    default_image_url = "https://previews.123rf.com/images/fokaspokas/fokaspokas1805/fokaspokas180500401/102128105-simple-mobile-phone-icon-linear-symbol-thin-outline.jpg"
     name = 'product'
--- product_backend_api/product/models.py
+++ product_backend_api/product/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from .apps import ProductConfig
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory_quantity = models.IntegerField()
+    image = models.CharField(max_length=510, default=ProductConfig.default_image_url)
